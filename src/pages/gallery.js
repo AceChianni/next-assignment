07732232dc09332c45349af52c09098bb1ed2b70
Ace LaCabe <150067821+AceChianni@users.noreply.github.com>
2024-10-15 03:58:06 +0000

    added nav bar to gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 import styles from '../styles/Gallery.module.css';
+
 
 const Gallery = () => {
   return (
-    <div className={`container ${styles.galleryContainer}`}>
-      <h1 className={styles.title}>Gallery</h1>
-      <div className="row">
-        {[...Array(6)].map((_, index) => (
-          <div className="col-md-4 mb-4" key={index}>
-            <div className={styles.imageWrapper}>
-              <img 
-                src={`/images/gallery/personal-img${index + 1}.jpg`} 
-                alt={`Personal Image ${index + 1}`} 
-                className="img-fluid"
-              />
+    <div>
+      <Navbar /> 
+      <div className={`container ${styles.galleryContainer}`}>
+        <h1 className={styles.title}>Gallery</h1>
+        <div className="row">
+          {[...Array(6)].map((_, index) => (
+            <div className="col-md-4 mb-4" key={index}>
+              <div className={styles.imageWrapper}>
+                <img 
+                  src={`/images/gallery/personal-img${index + 1}.jpg`} 
+                  alt={`Personal Image ${index + 1}`} 
+                  className="img-fluid"
+                />
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
