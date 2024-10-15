95449e896bd86f3e43a0f59d780ecea0459e50a1
Ace LaCabe <150067821+AceChianni@users.noreply.github.com>
2024-10-15 03:01:46 +0000

    edit home.css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,3 @@
-// // src/pages/index.js
-// import Link from 'next/link';
-// import Image from 'next/image';
-// import Navbar from '../components/Navbar';
-// import styles from '../styles/Home.module.css';
-// import Counter from '../components/Counter';
-// import UserForm from '../components/UserForm';
-
-// export default function Home() {
-//   return (
-//     <div className={styles.container}>
-//       <Navbar /> {/* Adds Navbar */}
-//       <h1 className={styles.title}>✨ Welcome to Anni's Odyssey! ✨</h1>
-//       <p className={styles.description}>
-//         Here, we celebrate artistic expression, inclusivity, and wellness. Join me on a journey of creativity and self-discovery!
-//       </p>
-//       <p className={styles.quote}>
-//         “Art enables us to find ourselves and lose ourselves at the same time.” – Thomas Merton
-//       </p>
-
-// <Counter />
-// <UserForm />
-//     </div>
-//   );
-// }
-
-
 import Link from 'next/link';
 import Image from 'next/image';
 import Navbar from '../components/Navbar';
